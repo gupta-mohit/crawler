95a8933385a40421ec598bc87c7cdc4d7a42dbf6
Mohit gupta <mohit.gupta@imaginea.com>
2015-06-25 12:41:14 +0530

    #moving application constants to read from property file

--- src/main/java/com/mohit/crawler/StartMailCrawler.java
+++ src/main/java/com/mohit/crawler/StartMailCrawler.java
@@ -1,10 +1,45 @@
 package com.mohit.crawler;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class StartMailCrawler {
 
 	public static void main(String args[]){
 
-		WebCrawler mailCrawler =new MailCrawler("http://mail-archives.apache.org/mod_mbox/maven-users/","2015","C:\\Users\\mohit\\Documents\\mails");
+		Properties prop = new Properties();
+		InputStream input = null;
+		String baseUrl="";
+		String year="";
+		String path="";
+
+		try {
+
+			input = new FileInputStream("LinkCrawler.properties");
+
+			// load a properties file
+			prop.load(input);
+
+			 baseUrl=prop.getProperty("baseUrl");
+			 year=prop.getProperty("year");
+			 path=prop.getProperty("pathToStoreMail");
+			
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		WebCrawler mailCrawler =new MailCrawler(baseUrl,year,path);
 		mailCrawler.crawl();
 		mailCrawler.processCrawlData();
 	}
