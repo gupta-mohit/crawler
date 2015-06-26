0340479c5d928bae10b2dcf7b163c3af6791f9a8
Mohit gupta <mohit.gupta@imaginea.com>
2015-06-26 16:28:27 +0530

    #Removing unused imports and commented code

--- src/main/java/com/mohit/crawler/StartMailCrawler.java
+++ src/main/java/com/mohit/crawler/StartMailCrawler.java
@@ -1,10 +1,5 @@
 package com.mohit.crawler;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,38 +8,9 @@ import com.mohit.crawler.constants.MailCrawlerPropertiesBean;
 public class StartMailCrawler {
 
 	public static void main(String args[]){
-		
-		ApplicationContext context = new ClassPathXmlApplicationContext(
-				"MailLinkCrawlerBeans.xml");
-
+		ApplicationContext context = new ClassPathXmlApplicationContext("MailLinkCrawlerBeans.xml");
 		MailCrawlerPropertiesBean mailCrawlerpropertiesBean = (MailCrawlerPropertiesBean) context.getBean("mailLinkCrawlerProperties");
 
-		
-		/*
-
-		Properties prop = new Properties();
-		InputStream input = null;
-		String linkToCrawl="";
-
-		try {
-
-			input = new FileInputStream("LinkCrawler.properties");
-			// load a properties file
-			prop.load(input);
-			linkToCrawl=prop.getProperty("linkToCrawl");
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-*/
 		WebCrawler mailCrawler =new MailCrawler(mailCrawlerpropertiesBean.getLinkToCrawl());
 		mailCrawler.crawl();
 		mailCrawler.processCrawlData();
